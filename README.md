0b5259f2e1853cab64f1d9accc187b465105e2e7
mehulghosal <mehul.ghosal@gmail.com>
2018-11-07 18:18:48 -0500

    updated instructions.txt to include git stuff

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # LeadZeppelin
 CSII project lead zeppelin
+
+our goal here is to create an automated guitar player
+this project is a continuation of Alden Lamp, David Shustin, Claire Yan, and Jaimin Patel's submission to PenApps Fall 2018
+i decided to continue this bc i thought it was rly cool
+
+ok as of 11/7, j go into the practice folder, instructions will be in insturctions.txt
--- practice/mehul.py
+++ practice/mehul.py
@@ -11,12 +11,21 @@ def fizzbuzz():
 
 		print(output)
 
-def primes():
+def findPrimes():
 	n = int(input("enter upper bound: "))
 	if n <=1: 
 		print("invalid upper bound")
 	else:
-		for i in range(2, n):
-			for j in range(2, i):
-				i%j==0:
+		for i in range(2, n+1):
+			if checkPrime(i):
+				print(i)
 					
+
+def checkPrime(n):
+	for i in range(2, n):
+		if n%i==0:
+			return False
+	return True
+
+fizzbuzz()
+findPrimes()
