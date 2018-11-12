a41bfc12ddcf4e61ab242144e212678b0b507b62
Aoife Weiss <aoifeweiss1@Aoifes-MacBook-Air.local>
2018-11-11 23:14:54 -0500

    changed aoife.py

---
+++ practice/aoife.py
@@ -0,0 +1,22 @@
+for i in range(1,101):
+	if (i%15==0):
+		print("fizzbuzz")
+	elif (i%5==0):
+		print("buzz")
+	elif (i%3==0):
+		print("fizz")
+	elif (i%1==0):
+		print(i)
+	elif (i%2==0):
+		print(i)
+	elif (i%4==0):
+		print(i)
+	elif (i%6==0):
+		print(i)
+	elif (i%7==0):
+		print(i)
+	elif (i%8==0):
+		print(i)
+	elif (i%9==0):
+		print(i)
+		
