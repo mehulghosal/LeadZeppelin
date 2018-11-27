454314fd0c3f6c7c0e426c18515bb834c6651dd7
aoifeweiss <aoifeweiss@gmail.com>
2018-11-27 00:48:33 -0500

    fIBoNaCcI

--- practice/aoife.py
+++ practice/aoife.py
@@ -5,29 +5,48 @@ for i in range(1,101):
 		print("buzz")
 	elif (i%3==0):
 		print("fizz")
-	elif (i%1==0):
-		print(i)
-	elif (i%2==0):
-		print(i)
-	elif (i%4==0):
-		print(i)
-	elif (i%6==0):
-		print(i)
-	elif (i%7==0):
-		print(i)
-	elif (i%8==0):
-		print(i)
-	elif (i%9==0):
-		print(i)
-
-num = int(input("Enter a number"))
-if (num < 1):
-	print("YouFudgedUpBro")
-if (num > 1):
-	for i in range (2,num):
-		if (num % i) == 0:
-			print (num, "ce n'est pas un prime number")	
-			print (i, "times", num//i, "is", num)
 	else:
-		print(num, "ITS A PRIME NUMERO Senoritas")
+		print(i)
+	# Mehul's design
+	#s:""
+	#	if i%3==0:
+	#		s+="fizz"
+	#	if i%5==0:
+	#		s+=Str(i)
+	#	print(s)
 
+	for n in range(2,101):
+		for a in range (0,0):
+			if (n%2)==1:
+				print(a)
+			if (n%3)==1:
+				print(a)
+			if (n%4)==1:
+				print(a)
+			if (n%5)==1:
+				print(a)
+			if (n%6)==1:
+				print(a)
+			if (n%7)==1:
+				print(a)
+			if (n%8)==1:
+				print(a)
+			if (n%9)==1:
+				print(a)
+			else:
+				print(n)
+
+#for i in range (2,101):
+#	for j in range (1,i):
+# write all the divisible shit
+
+def F(g):
+	if g == 0:
+		return 0
+	elif g == 1:
+		return 1
+	return F(g-1) + F(g-2)
+
+for l in range(0,30):
+	print (F(l))
+ 
