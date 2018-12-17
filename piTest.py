17aa3276319e0290341b6941655d845ca7cf2550
mehulghosal <mehul.ghosal@gmail.com>
2018-12-17 14:10:37 -0500

    servo code

--- piTest.py
+++ piTest.py
@@ -1,38 +1,11 @@
 import time
-'''
-t = time.time()
-print("hello world")
-
-for i in range(5000):
-	print(i)
-
-print(time.time() - t)
-'''
 
 #doc : https://gpiozero.readthedocs.io/en/latest/index.html
 import gpiozero as gpio
-l = gpio.LED(21)
-while True:
-    l.on()
-    time.sleep(1);
-    l.off()
-    time.sleep(1);
 
-'''
-#this does not work
-import RPIO.GPIO as gpio
-gpio.setmode(gpio.board)
-
-servo = GPIO.PWM(12, 50)
-servo.start()
-
-try:
-	while True:
-		servo.changeDutyCycle(7.5)
-		time.sleep(1)
-		servo.changeDutyCycle(2.5)
-		time.sleep(1)
-except KeyboardInterrupt:
-	servo.stop()
-	GPIO.cleanup()
-'''
+s = gpio.AngularServo(2, min_angle = 0, max_angle = 180)
+while(True):
+    s.min()
+    time.sleep(3)
+    s.max()
+    time.sleep(3)
