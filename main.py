908bfe8f7558830d34ca9d715d423859c400b309
mehulghosal <mehul.ghosal@gmail.com>
2018-12-20 00:09:54 -0500

    god i hate github added try catch for inputs

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ from picking import pick
 #takes in keyboard intakes (0-6)
 def play():
 	while True:
-		inp = int(input("enter number for string (0 for strum): "))
-		pick(inp)
+		try:
+			inp = int(input("enter number for string (0 for strum): "))
+			pick(inp)
+		except Exception:
+			continue
+		
 
 if __name__ == 'main':
 	play()
