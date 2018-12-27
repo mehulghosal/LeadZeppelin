3cc55bc794dad32ec674c4f58c4a058d7c2b14bd
mehulghosal <mehul.ghosal@gmail.com>
2018-12-27 12:29:24 -0500

    working wiring testing all servos at same time

--- piTest.py
+++ piTest.py
@@ -1,12 +1,10 @@
-import time
-
+from time import sleep
+import picking
 #doc : https://gpiozero.readthedocs.io/en/latest/index.html
-import gpiozero as gpio
 
-s = gpio.AngularServo(2, min_angle = 0, max_angle = 180)
 while(True):
-	s.min()
-	time.sleep(3)
-	s.max()
-	time.sleep(3)
+	for i in range(7):
+            picking.pick(i)
+            sleep(0.5)
+        sleep(2)
 
--- picking.py
+++ picking.py
@@ -13,7 +13,7 @@ STRUMMINANGLE = -30
 #INIT SERVOS
 #index 0 is strummer servo
 #indices 1-6 are indiv strings high to low
-servos = [gpio.AngularServo(18, initial_angle=0, min_angle= STRUMMINANGLE, max_angle=STRUMMAXANGLE), gpio.AngularServo(3, initial_angle=0, min_angle=MINANGLE, max_angle=MAXANGLE), gpio.AngularServo(4, initial_angle=0, min_angle=MINANGLE, max_angle=MAXANGLE), gpio.AngularServo(5, initial_angle=0, min_angle=MINANGLE, max_angle=MAXANGLE), gpio.AngularServo(6, initial_angle=0, min_angle=MINANGLE, max_angle=MAXANGLE), gpio.AngularServo(7, initial_angle=0, min_angle=MINANGLE, max_angle=MAXANGLE), gpio.AngularServo(8, initial_angle=0, min_angle=MINANGLE, max_angle=MAXANGLE)]
+servos = [gpio.AngularServo(21, initial_angle=0, min_angle= STRUMMINANGLE, max_angle=STRUMMAXANGLE), gpio.AngularServo(20, initial_angle=0, min_angle=MINANGLE, max_angle=MAXANGLE), gpio.AngularServo(4, initial_angle=0, min_angle=MINANGLE, max_angle=MAXANGLE), gpio.AngularServo(5, initial_angle=0, min_angle=MINANGLE, max_angle=MAXANGLE), gpio.AngularServo(6, initial_angle=0, min_angle=MINANGLE, max_angle=MAXANGLE), gpio.AngularServo(7, initial_angle=0, min_angle=MINANGLE, max_angle=MAXANGLE), gpio.AngularServo(8, initial_angle=0, min_angle=MINANGLE, max_angle=MAXANGLE)]
 for i in servos: i.min()
 
 #returns list of angle values in order servos are defined
