c08bfa321146c6fb1255c11d7b726f142e4529fa
SilverSlipstream <aemb011@gmail.com>
2019-01-15 14:38:53 -0500

    parser can now get tempo value

--- musicTest/parseMidi.py
+++ musicTest/parseMidi.py
@@ -1,11 +1,15 @@
+import mido
 from mido import MidiFile
 mid = MidiFile('Bach_My_Heart_Ever_Faithful_BWV34.mid')
 for i, track in enumerate(mid.tracks):
-  print('Track {}: {}'.format(i, track.name))
-  for msg in track:
-  	m = str(msg)
-    if "set_tempo" in m:
-    	strAfterTempo = m.str(m.index("tempo=",0))
-    	print(strAfterTempo);
-    elif "note_on" in m:
-    	print(m);
+	print('Track {}: {}'.format(i, track.name))
+	for msg in track:
+		m = "a"
+		m = str(msg)
+		if msg.type == 'set_tempo':
+			indexOfTempo = m.index("tempo=",0)+6
+			indexOfSpace = m.index(" ",indexOfTempo)+0
+			tempo = m[indexOfTempo:indexOfSpace]
+			print(tempo)
+		#elif msg.type == "note_on":
+			#print(m)
