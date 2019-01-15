e1287189c532acb72b0e269ca7f025373ef3538a
SilverSlipstream <aemb011@gmail.com>
2019-01-15 11:32:36 -0500

    changes parseMidi with ifs

--- musicTest/parseMidi.py
+++ musicTest/parseMidi.py
@@ -1,6 +1,11 @@
 from mido import MidiFile
-mid = MidiFile('song.mid')
+mid = MidiFile('Bach_My_Heart_Ever_Faithful_BWV34.mid')
 for i, track in enumerate(mid.tracks):
   print('Track {}: {}'.format(i, track.name))
   for msg in track:
-    print(msg)
+  	m = str(msg)
+    if "set_tempo" in m:
+    	strAfterTempo = m.str(m.index("tempo=",0))
+    	print(strAfterTempo);
+    elif "note_on" in m:
+    	print(m);
