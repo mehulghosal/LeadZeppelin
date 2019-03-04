cce8d30bf84df646f6c8940e574a3a9a0d1ca5d5
mehulghosal <mehul.ghosal@gmail.com>
2019-03-04 12:06:08 -0500

    just need to debug last two picking servos

--- python/piTest.py
+++ python/piTest.py
@@ -3,7 +3,7 @@ import picking
 #doc : https://gpiozero.readthedocs.io/en/latest/index.html
 
 while(True):
-    for i in range(7):
+    for i in range(6):
         picking.pick(i)
         sleep(0.5)
     sleep(2)
--- python/picking.py
+++ python/picking.py
@@ -1,21 +1,18 @@
 import gpiozero as gpio
 
 #max and min
-MAXANGLE = 7.5
-MINANGLE = -7.5
-
-STRUMMAXANGLE = 15
-STRUMMINANGLE = -15
+MAXANGLE = 1
+MINANGLE = -1
 
 #pin numbers are temporary - change as needed
 #angle values are temporary - change as needed
 
 #INIT SERVOS
-#index 0 is strummer servo
-#indices 1-6 are indiv strings high to low
-servos = [gpio.AngularServo(21, initial_angle=0, min_angle= STRUMMINANGLE, max_angle=STRUMMAXANGLE), gpio.AngularServo(20, initial_angle=0, min_angle=MINANGLE, max_angle=MAXANGLE), gpio.AngularServo(4, initial_angle=0, min_angle=MINANGLE, max_angle=MAXANGLE), gpio.AngularServo(5, initial_angle=0, min_angle=MINANGLE, max_angle=MAXANGLE), gpio.AngularServo(6, initial_angle=0, min_angle=MINANGLE, max_angle=MAXANGLE), gpio.AngularServo(7, initial_angle=0, min_angle=MINANGLE, max_angle=MAXANGLE), gpio.AngularServo(8, initial_angle=0, min_angle=MINANGLE, max_angle=MAXANGLE)]
+
+servos = [gpio.AngularServo(21, initial_angle=0, min_angle= MINANGLE, max_angle=MAXANGLE), gpio.AngularServo(20, initial_angle=0, min_angle=MINANGLE, max_angle=MAXANGLE), gpio.AngularServo(4, initial_angle=0), gpio.AngularServo(5, initial_angle=0, min_angle=MINANGLE, max_angle=MAXANGLE), gpio.AngularServo(6, initial_angle=0, min_angle=MINANGLE, max_angle=MAXANGLE), gpio.AngularServo(7, initial_angle=0, min_angle=MINANGLE, max_angle=MAXANGLE)]
 for i in servos: i.min()
 
+s = [0, 0, 0, 0, 0, 0]
 #returns list of angle values in order servos are defined
 def state():
     s = []
@@ -29,18 +26,12 @@ def state():
 def pick(string):
     s = state()[string]
     ang = 0
-    #Strumming
-    if string == 0:
-        if s == STRUMMAXANGLE:
-            ang = STRUMMINANGLE
-        elif s == STRUMMINANGLE:
-            ang = STRUMMAXANGLE
     #Indiv strings
-    else:
-        if s == MAXANGLE:
-            ang = MINANGLE
-        elif s == MINANGLE:
-            ang = MAXANGLE
+    if s == MAXANGLE:
+        ang = MINANGLE
+    elif s == MINANGLE:
+        ang = MAXANGLE
 
     servos[string].angle = ang
     return ang
+
