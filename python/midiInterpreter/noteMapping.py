2724c6643a0ae1a9380d725a81cec73effb2d5dd
SilverSlipstream <aemb011@gmail.com>
2019-03-07 14:37:55 -0500

    started separating playNotes and parseMidi into functions

--- python/midiInterpreter/noteMapping.py
+++ python/midiInterpreter/noteMapping.py
@@ -13,7 +13,16 @@
 # 10: "A#/Bb",
 # 11: "B"
 
-guitarStrings = [[],[],[],[],[],[]] #6 empty arrays that will contain notes that can be played on each string
+import parseMidi as midi
+
+guitarStrings = [[],[],[],[],[],[]] #will contain all possible notes on each string
+
+chords = [] #holds parsed chords
+tempoChanges = [] #holds parse tempo changes
+
+railPos = [ [10,10,10,10,10,10,0] ] #starting positions of each of the pushers (10 is arbitrary right now)
+									#list in a list because holds other rail positions
+pickStrings = [] #holds which strings are picked each chord
 
 def tune(tuning = [(4,2), (10,2), (2,3), (7,3), (11,3), (4,4)]): #sets default tuning strings
 	for i in range(6):
@@ -32,3 +41,42 @@ def checkNote(noteTuple):
 		if noteTuple in guitarStrings[i]:
 			possibleStrings[i] = guitarStrings[i].index(noteTuple) #add a tuple with which string and the note's position on it
 	return possibleStrings
+
+def getMusic(chosenSong):
+	returnedStuff = midi.parse(chosenSong) #returns a tuple, consisting of an array of tuples, and an array of arrays of tuples
+	tempoChanges = returnedStuff[0]
+	chords = returnedStuff[1]
+	for c in chords: #example: [(0, 4), (9, 3), 120]
+		availableStrings = [0,1,2,3,4,5] #strings that can be played, 0 lowest 5 is highest
+		stringsToPick = [] #to tell which strings to be picked
+		newRailPos = []
+		for t in reversed(range(len(c)-1)): #for each tuple item, excluding the time, backwards.
+											#this is because the parseMidi formats the notes highest first, and
+											#lowest must be prioritized. Therefore, last note before the time value
+											#is the lowest note, and start there
+
+			rawPosStrings = checkNote(c[t]) #checks which strings the note can be played on
+			relativeDist = {}
+			for k, v in rawPosStrings.items(): #for each string(k), fret(v) in all possible string positions
+				if k in availableStrings: #string isn't playing another note
+					if v == 0: #prioritizing open string
+						relativeDist[k] = -1 #so it's better to do open than playing the same note
+					else:
+						mostRecentRailPositions = railPos[len(railPos)-1] #the latest rail positions
+						posOfString = mostRecentRailPositions[k] #get the position of pusher on string k
+						relativeDist[k] = abs(v - positionOfString) #distance from pusher to note's fret
+
+			lowestString = min(relativeDist, key=relativeDist.get) #string with lowest distance
+			lowestPos = rawPosStrings.get(lowestString) #actual lowest position
+			newRailPos.append()
+			availableStrings.remove(lowestString) #the string is going to be playing a note
+			stringsToPick.append(lowestString) #pick the string
+		newRailPos.append(c.get(len(c)-1)) #append the time of the chord
+		railPos.append(newRailPos) #put the new rail position into list of all of them
+		pickStrings.append(stringsToPick) #put the strings to be picked in the list
+
+def getPickStrings():
+	return chords
+
+def getRailPos(tuning):
+	return railPos
--- python/midiInterpreter/parseMidi.py
+++ python/midiInterpreter/parseMidi.py
@@ -13,30 +13,37 @@ def calcOctave(noteName):
 			break
 	return (noteName, octave)
 
+def rawParse(midiName): #only for debugging. Prints all messages
+	mid = mido.MidiFile(midiName)
+	for i, track in enumerate(mid.tracks):
+		print(track)
+		for msg in track:
+			print(msg)
 
 #midiName - midi file as a string 
 #returns list of chords
 #each chord is a list of tuples, last of which is the time duration of note
 def parse(midiName):
 	mid = mido.MidiFile(midiName)
+	data = []
 
+	if True: #replace with no note offs
+		data = parseNoOffs()
+	else:
+		data = parseYesOffs()
+	return data
+	
+def parseYesOffs():
 	chords = [] #to hold all the chords
 	chordNotes = [] #to track the notes parsed. example list: [(4,1),(7,1),("time",120)] -> play E1 & G1 for 120ms
 	tempoChanges = []
-
 	firstOn = False
 	firstOff = True
 	for i, track in enumerate(mid.tracks):
 		for msg in track:
 			m = str(msg)
 			if msg.type == "set_tempo":
-				indexOfTempo = m.index("tempo=",0)+6
-				indexOfSpace = m.index(" ",indexOfTempo)
-				tempo = m[indexOfTempo:indexOfSpace]
-				indexOfTime = m.index("time=",0)+5
-				indexOfGTS = m.index(">",0) #GTS = greater-than sign or '>'
-				time = m[indexOfTime:indexOfGTS]
-				
+				parseTempo(m)
 				tempoChanges.append((int(tempo),int(time)))
 			else:
 				if msg.type == "note_on":
@@ -63,3 +70,43 @@ def parse(midiName):
 						firstOn = True
 
 	return tempoChanges, chords
+
+def parseNoOffs():
+	chords = [] #to hold all the chords
+	chordNotes = [] #to track the notes parsed. example list: [(4,1),(7,1),("time",120)] -> play E1 & G1 for 120ms
+	tempoChanges = []
+	firstOn = False
+	firstOff = True
+	for i, track in enumerate(mid.tracks):
+		for msg in track:
+			m = str(msg)
+			if msg.type == "set_tempo":
+				parseTempo(m)
+				tempoChanges.append((int(tempo),int(time)))
+			elif msg.type == "note_on":
+				indexOfTime = m.index("time=",0)+5
+				time = m[indexOfTime:]
+				time = int(time)
+
+				indexOfNote = m.index("note=",0)+5
+				indexOfSpace = m.index(" ",indexOfNote)
+				note = m[indexOfNote:indexOfSpace]
+				chordNotes.append(calcOctave(int(note)))
+				if time != 0:
+					chordNotes.append(time)
+					chord = []
+					chord.extend(chordNotes)
+					chords.append(chord) #add the 'packaged' chord to the chords
+					chordNotes.clear()
+
+	return tempoChanges, chords
+
+def parseTempo(message):
+	indexOfTempo = m.index("tempo=",0)+6
+	indexOfSpace = m.index(" ",indexOfTempo)
+	tempo = m[indexOfTempo:indexOfSpace]
+				
+	indexOfTime = m.index("time=",0)+5
+	indexOfGTS = m.index(">",0) #GTS = greater-than sign or '>'
+	time = m[indexOfTime:indexOfGTS]
+	return tempo, time
