073f3f0b8488bd6c98adc3a0afe91a1f559418f3
SilverSlipstream <aemb011@gmail.com>
2019-03-08 14:25:42 -0500

    Streamlined functions and other files

---
+++ python/main.py
@@ -0,0 +1,22 @@
+import playNotes as play
+import noteMapping as note
+
+play.getSongs()
+song = play.pickSong()
+
+note.tune()
+data = getMusic(song)
+tempoChanges = data[0]
+railPos = data[1]
+pickStrings = data[2]
+
+for i in range(len(pickStrings)):
+	#wait railPos[i+1][6] ticks?
+	
+	#thread 1:
+	for j in railPos[i+1]:
+		#pusher 1 to position railPos[i+1][0]
+
+	#thread 2:
+	for k in pickStrings[i]: #find a way to pick them at the same time?
+		pick(k)
--- python/midiInterpreter/noteMapping.py
+++ python/midiInterpreter/noteMapping.py
@@ -17,13 +17,6 @@ import parseMidi as midi
 
 guitarStrings = [[],[],[],[],[],[]] #will contain all possible notes on each string
 
-chords = [] #holds parsed chords
-tempoChanges = [] #holds parse tempo changes
-
-railPos = [ [10,10,10,10,10,10,0] ] #starting positions of each of the pushers (10 is arbitrary right now)
-									#list in a list because holds other rail positions
-pickStrings = [] #holds which strings are picked each chord
-
 def tune(tuning = [(4,2), (10,2), (2,3), (7,3), (11,3), (4,4)]): #sets default tuning strings
 	for i in range(6):
 		noteCounter = tuning[i][0]
@@ -43,21 +36,25 @@ def checkNote(noteTuple):
 	return possibleStrings
 
 def getMusic(chosenSong):
-	returnedStuff = midi.parse(chosenSong) #returns a tuple, consisting of an array of tuples, and an array of arrays of tuples
+	railPos = [ [10,10,10,10,10,10,0] ] #starting positions of each of the pushers (10 is arbitrary right now)
+										#list in a list because holds other rail positions
+	pickStrings = [] #holds which strings are picked each chord
+
+	returnedStuff = midi.parse(chosenSong)
 	tempoChanges = returnedStuff[0]
 	chords = returnedStuff[1]
 	for c in chords: #example: [(0, 4), (9, 3), 120]
 		availableStrings = [0,1,2,3,4,5] #strings that can be played, 0 lowest 5 is highest
-		stringsToPick = [] #to tell which strings to be picked
+		stringsToPick = []
 		newRailPos = []
 		for t in reversed(range(len(c)-1)): #for each tuple item, excluding the time, backwards.
-											#this is because the parseMidi formats the notes highest first, and
+											#this is because parseMidi formats the notes highest first, and
 											#lowest must be prioritized. Therefore, last note before the time value
 											#is the lowest note, and start there
 
 			rawPosStrings = checkNote(c[t]) #checks which strings the note can be played on
 			relativeDist = {}
-			for k, v in rawPosStrings.items(): #for each string(k), fret(v) in all possible string positions
+			for k, v in rawPosStrings.items(): #for string(k), fret(v) in all possible string positions
 				if k in availableStrings: #string isn't playing another note
 					if v == 0: #prioritizing open string
 						relativeDist[k] = -1 #so it's better to do open than playing the same note
@@ -71,12 +68,9 @@ def getMusic(chosenSong):
 			newRailPos.append()
 			availableStrings.remove(lowestString) #the string is going to be playing a note
 			stringsToPick.append(lowestString) #pick the string
+
 		newRailPos.append(c.get(len(c)-1)) #append the time of the chord
 		railPos.append(newRailPos) #put the new rail position into list of all of them
 		pickStrings.append(stringsToPick) #put the strings to be picked in the list
 
-def getPickStrings():
-	return chords
-
-def getRailPos(tuning):
-	return railPos
+	return tempoChanges, railPos, pickStrings
